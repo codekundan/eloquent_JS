308dfc935778aafb96f708fcfd9e6f20ec1926eb
codekundan <codekundannegi@gmail.com>
2022-03-04 13:55:42 +0530

    chapter 2 FizzBuzz program

---
+++ 2.2.js
@@ -0,0 +1,18 @@
+// FizzBuzz
+
+
+for( let i = 0; i <= 99 ; i++) {
+    if ( ((i+1)%3 == 0) && ((i+1)%5 == 0) ) {
+        console.log("FizzBuzz");
+    }
+    
+    else if( (i+1)%3 == 0 ) {
+        console.log("Fizz");
+    } 
+    else if((i+1)%5 == 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log((i+1));
+    }
+}
