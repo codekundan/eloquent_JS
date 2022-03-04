650ccb8429d3c96cf904771808458e1671ebecf6
codekundan <codekundannegi@gmail.com>
2022-03-04 12:59:39 +0530

    chapter 1 first program

---
+++ 2.1.js
@@ -0,0 +1,12 @@
+// Method 1
+
+// for( let i = 0; i<=6 ; i++) {
+//     console.log("#".repeat(i));
+// }
+
+
+//  Method 2
+
+for( let i = "#"; i.length <= 7 ; i = i + "#") {
+    console.log(i);
+}
