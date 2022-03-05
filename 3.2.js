e1796a6df884f915e3b17ab418968268449cb210
codekundan <codekundannegi@gmail.com>
2022-03-05 22:43:07 +0530

    updated for negative num

--- 3.2.js
+++ 3.2.js
@@ -3,7 +3,9 @@
 function isEven(num) {
     if(num==0) return true;
     else if(num==1) return false;
+    else if(num<0) return isEven(-num);
     else return isEven(num-2);
 }
-console.log(isEven(253));
-console.log(isEven(256));
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-2));
