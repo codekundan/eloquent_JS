dc23bcbc0900874f2d284a57f67fde5560abc09e
codekundan <codekundannegi@gmail.com>
2022-03-10 13:34:55 +0530

    comment changes

--- 5.1.js
+++ 5.1.js
@@ -2,7 +2,7 @@ let arr = [[1,2,3],[4,5],[6]];
 
 console.log(arr.reduce((previousValue, currentValue) => previousValue.concat(currentValue), [10,9]));
 
-// previousValue takes the empty array [], which is given as initialValue. 
-// If no initialValue given, then previousValue takes first value at array index 0. 
+// previousValue takes the array [10,9], which is given as initialValue.
 // currentValue takes the first value of array at index 0.
+// If no initialValue given, then previousValue takes first value at array index 0. 
 //  If no initialValue given, then currentValue takes second value at array index 1. 
